29dfc3900a196443bc9d5af8fbc4e3693c6e6664
Arijit Paria <arijitparia2020@gmail.com>
2024-02-16 13:13:47 +0530

    go lang init + variables

---
+++ 02Variables/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Variables")
+	var username string = "Arijit"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n\n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("isLoggedIn is of type: %T \n\n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Type of smallval: %T\n\n", smallVal)
+
+	var smallFloat float32 = 255.47348327432
+	fmt.Println(smallFloat)
+	fmt.Printf("smallFloat is of Type: %T\n\n", smallFloat)
+
+	//defult values and some aliases
+	var anothervalue int
+	fmt.Println(anothervalue)
+	fmt.Printf("Type of anothervarible is: %T\n\n", anothervalue)
+
+	var website = "TUTORIEX"
+	fmt.Println(website)
+
+	//no var style
+	numberofuser := 4000
+	fmt.Println(numberofuser)
+
+}
